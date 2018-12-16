ba0cc070f4ee24dc52b483eab5b13b6878dbc83f
Deepak Chethan <deepakchethan@outlook.com>
2018-12-16 18:49:48 +0530

    mongo configure

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+
 app.use('/', indexRouter);
 app.use('/form', passport.authenticate('jwt', {session: false}), protectedRouter);
 
--- config/database.js
+++ config/database.js
@@ -4,6 +4,6 @@
 module.exports = {
   'secret':'nodeauthsecret',
   'database': 'ds135724.mlab.com:35724/heroku_f4nkcmkx',
-  'database': 'mongodb://eroku_f4nkcmkx:Idontknowmypass123@ds135724.mlab.com:35724/heroku_f4nkcmkx',
+  'database': 'mongodb://deepak:Idontknowmypass123@ds135724.mlab.com:35724/heroku_f4nkcmkx',
   'connection':null
 };
