de10f3d4afdc3e5af3468abd868bd2fa31c3c8c8
Deepak Chethan <deepakchethan@outlook.com>
2018-12-16 22:16:13 +0530

    Added the sendgrid mail reset mech

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,8 @@ var passport = require('passport');
 require('../config/passport')(passport);
 var jwt = require('jsonwebtoken');
 var mongoose = require('mongoose');
+var async = require('async');
+var crypto = require('crypto');
 
 var config = require('../config/database')
 
@@ -55,11 +57,66 @@ router.post('/signup/',function(req,res,next){
   }
 });
 
-router.post('/forgotPassword', function (req, res, next) {
-  if (!req.body.email) {
-    res.json({success: false, msg:'Email is necessary'});
-  }
-  // TODO handle this
+router.get('/forgotPassword', function (req, res, next) {
+  async.waterfall([
+    function(done) {
+
+      crypto.randomBytes(20, function(err, buf) {
+        var token = buf.toString('hex');
+        done(err, token);
+      });
+    },
+    function(token, done) {
+      User.findOne({ email: req.body.email }, function(err, user) {
+        if (!user) {
+          res.json({'success': false});
+        }
+
+        user.resetPasswordToken = token;
+        user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
+
+        user.save(function(err) {
+          done(err, token, user);
+        });
+      });
+    },
+    function(token, user, done) {
+      var smtpTransport = nodemailer.createTransport('SMTP', {
+        service: 'SendGrid',
+        auth: {
+          user: 'bhumirte',
+          pass: 'Idontknowmypass'
+        }
+      });
+      var mailOptions = {
+        to: user.email,
+        from: 'passwordreset@bhumirte.com',
+        subject: 'Bhumi RTE Password Reset',
+        text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
+          'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
+          'http://' + req.headers.host + '/reset/' + token + '\n\n' +
+          'If you did not request this, please ignore this email and your password will remain unchanged.\n'
+      };
+      smtpTransport.sendMail(mailOptions, function(err) {
+        
+      });
+    }
+  ], function(err) {
+    if (err) return next(err);
+    res.redirect('/forgot');
+  });
+});
+
+app.get('/reset/:token', function(req, res) {
+  User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, function(err, user) {
+    if (!user) {
+      
+      return res.redirect('/forgot');
+    }
+    res.render('reset', {
+      user: req.user
+    });
+  });
 });
 
 module.exports = router;
